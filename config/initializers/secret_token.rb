b6fdbf366ee23c41fddd239601c66783d582818b
Mary-Anne Cosgrove <mcosgro@thoughtworks.com>
2012-11-26 10:52:04 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OurChorus::Application.config.secret_token = '82726ccd14a146df91d93234ac3032f1a7757502999070a29fb2f84f5f1e3825c3e7eef8110b0e38ab9cc3dc6944c91806b77b25f7a988dbaff141861c7ed317'
