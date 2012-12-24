766de507356948b09380939730ac829806311c4d
Mary-Anne Cosgrove <mcosgro@thoughtworks.com>
2012-12-24 16:25:55 +1100

    Resetting staging configuration to correct(?) settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,54 +1,49 @@
 OurChorus::Application.configure do
 
   #logging and debugging
-  #config.active_support.deprecation = :notify
-  config.active_support.deprecation = :log
+  config.active_support.deprecation = :notify
+  #config.active_support.deprecation = :log
 
    # Caching
-  config.cache_classes = false
-  # config.consider_all_requests_local       = false
-  config.consider_all_requests_local       = true
-#  config.action_controller.perform_caching = true
-  config.action_controller.perform_caching = false
+  #config.cache_classes = false
+  config.consider_all_requests_local       = false
+  #config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = true
+  #config.action_controller.perform_caching = false
 
   # Action Mailer
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  #config.action_mailer.default_url_options = { :host => 'ourchorus_staging.herokuapp.com/' }
-  #ActionMailer::Base.delivery_method = :smtp
-  #ActionMailer::Base.smtp_settings = {
-  #  :address        => "smtp.sendgrid.net",
-  #  :port           => "25",
-  #  :authentication => :plain,
-  #  :user_name      => ENV['SENDGRID_USERNAME'],
-  #  :password       => ENV['SENDGRID_PASSWORD'],
-  #  :domain         => ENV['SENDGRID_DOMAIN']
-  #}
+  config.action_mailer.default_url_options = { :host => 'ourchorus_staging.herokuapp.com/' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 
   # Enable threaded mode
   # config.threadsafe!
 
   # Internationalisation
-  #config.i18n.fallbacks = true
+  config.i18n.fallbacks = true
 
 
    # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
   # Assets
-  config.assets.compress = false
-  #config.assets.compress = true
-  config.assets.debug = true
+  #config.assets.compress = false
+  config.assets.compress = true
+  #config.assets.debug = true
   #config.serve_static_assets = false
-  #config.serve_static_assets = true
-  # config.assets.initialize_on_precompile = false
-  # Do fallback to assets pipeline if a precompiled asset is missed
-  # config.assets.compile = true
+  config.serve_static_assets = true
+  config.assets.initialize_on_precompile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
-  # config.assets.digest = true
-
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  #config.assets.manifest = asset_manifest
+  config.assets.digest = true
 end
