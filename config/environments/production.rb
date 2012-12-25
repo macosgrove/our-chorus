310132f3667bd1340cfa156f36175a54817cd072
Mary-Anne Cosgrove <mcosgro@thoughtworks.com>
2012-12-25 17:10:19 +1100

    Update production environment to match staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,58 +1,21 @@
 OurChorus::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
-  config.cache_classes = true
+  #logging and debugging
+  config.active_support.deprecation = :notify
+  #config.active_support.deprecation = :log
 
-  # Full error reports are disabled and caching is turned on
+   # Caching
+  #config.cache_classes = false
   config.consider_all_requests_local       = false
+  #config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
+  #config.action_controller.perform_caching = false
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
-
-  # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
-
-  # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Actionmailer host setting
-  config.action_mailer.default_url_options = { :host => 'ourchorus_production.herokuapp.com' }
+  # Action Mailer
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => 'ourchorus_staging.herokuapp.com/' }
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
@@ -65,11 +28,22 @@ OurChorus::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
+  # Internationalisation
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
 
+   # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  # Assets
+  #config.assets.compress = false
+  config.assets.compress = true
+  #config.assets.debug = true
+  #config.serve_static_assets = false
+  config.serve_static_assets = true
+  config.assets.initialize_on_precompile = false
+  config.assets.compile = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 end
