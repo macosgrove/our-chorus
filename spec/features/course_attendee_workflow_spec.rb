1091ac2c1183cffca787c633445ff5849d47f5e9
Mary-Anne Cosgrove <maryannecos@gmail.com>
2013-07-01 21:16:10 +1000

    Add functional test for attendees

---
+++ spec/features/course_attendee_workflow_spec.rb
@@ -0,0 +1,31 @@
+require 'features/functional_helper'
+
+
+describe 'registered member workflow', :type => :request do
+
+  let!(:member) {FactoryGirl.create(:course_attendee, username: 'member', password: 'password', password_confirmation: 'password')}
+
+  before do
+    sign_in_with('member', 'password')
+  end
+
+  context 'the member has logged in' do
+    it 'should show navigational links' do
+      page.should have_link 'Our Vision'
+      page.should have_link 'Our Values'
+      page.should_not have_link 'Our Music'
+      page.should have_link 'Our Members'
+      page.should_not have_link 'Our Practices'
+      page.should_not have_link 'Sign in'
+      page.should have_link 'Sign out'
+      page.should_not have_link 'Join us!'
+      page.should have_link 'Change my Details'
+      page.should have_link 'Course Materials'
+    end
+
+    it 'should allow access to the course materials page' do
+      visit content_course_materials_path
+      page.should have_text 'Course Materials'
+    end
+  end
+end
